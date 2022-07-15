c0d6da64423d2cafa26b1999e244e81e78557ea1
Bruno Campos <brunosopmac@gmail.com>
2022-07-15 10:13:33 -0300

    Finished

---
+++ src/components/organisms/Presentation.vue
@@ -0,0 +1,40 @@
+<template>
+  <main class="presentation">
+    <SocialNetworks />
+    <img
+      class="presentation"
+      src="@/assets/images/presentation.svg"
+      alt="Apresentação"
+    />
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {} from '@/components/atoms'
+import { SocialNetworks } from '@/components/molecules'
+
+export default defineComponent({
+  components: {
+    SocialNetworks
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.presentation {
+  position: relative;
+  display: grid;
+  justify-content: center;
+  .presentation {
+    width: 80%;
+    justify-self: center;
+  }
+  .social-networks {
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
